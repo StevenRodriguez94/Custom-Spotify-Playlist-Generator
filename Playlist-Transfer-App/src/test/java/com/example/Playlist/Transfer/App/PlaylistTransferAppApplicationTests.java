9a17fe6c5e2554b1a8d9fb7df6bd2528e172d68e
Steven Rodriguez <125320506+StevenRodriguez94@users.noreply.github.com>
2024-07-31 20:37:24 -0400

    Add files via upload

---
+++ Playlist-Transfer-App/src/test/java/com/example/Playlist/Transfer/App/PlaylistTransferAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Playlist.Transfer.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistTransferAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
